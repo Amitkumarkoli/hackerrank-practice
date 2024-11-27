7f7418ea355aad42ae7bb5ba199fde95bb1631f8
Amitkumarkoli <akdev523@gmail.com>
2024-11-27 21:48:51 +0530

    VarArgs = simple addition question is solved

---
+++ java/Advanced/VarArgs.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// Simple Addition Question 
+
+class Add {
+    public void add(int... numbers) {
+        int sum = 0;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+            result.append(numbers[i]);
+            if (i < numbers.length - 1) {
+                result.append("+");
+            }
+        }
+
+        result.append("=").append(sum);
+        System.out.println(result);
+    }
+}
+
+
+public class VarArgs {
+
+    public static void main(String[] args) {
+       try{
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			int n1=Integer.parseInt(br.readLine());
+			int n2=Integer.parseInt(br.readLine());
+			int n3=Integer.parseInt(br.readLine());
+			int n4=Integer.parseInt(br.readLine());
+			int n5=Integer.parseInt(br.readLine());
+			int n6=Integer.parseInt(br.readLine());
+			Add ob=new Add();
+			ob.add(n1,n2);
+			ob.add(n1,n2,n3);
+			ob.add(n1,n2,n3,n4,n5);	
+			ob.add(n1,n2,n3,n4,n5,n6);
+			Method[] methods=Add.class.getDeclaredMethods();
+			Set<String> set=new HashSet<>();
+			boolean overload=false;
+			for(int i=0;i<methods.length;i++)
+			{
+				if(set.contains(methods[i].getName()))
+				{
+					overload=true;
+					break;
+				}
+				set.add(methods[i].getName());
+				
+			}
+			if(overload)
+			{
+				throw new Exception("Overloading not allowed");
+			}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	
+	
+
+}
